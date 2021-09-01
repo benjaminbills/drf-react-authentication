bd572095f041dac8560dca11312aea1c0dccbea9
Benjamin Obafemi <benjaminoba234@gmail.com>
2021-09-01 09:55:00 +0300

    add user model

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'auth',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -126,4 +126,4 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-AUTH_USER_MODEL = 'auth.User'
+AUTH_USER_MODEL = 'users.User'
